4074a59de8e1fa2489f11c5beeb47c3cfa83061a
Koloina01 <kolomaherison@gmail.com>
2026-03-31 16:44:29 +0300

    fix: reconfigure datasource

--- src/main/java/SpringBoot/td5/datasource/DataSourceConfig.java
+++ src/main/java/SpringBoot/td5/datasource/DataSourceConfig.java
@@ -2,20 +2,19 @@ package SpringBoot.td5.datasource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import javax.sql.DataSource;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.postgresql.ds.PGSimpleDataSource;
 
 @Configuration
 public class DataSourceConfig {
 
     @Bean
     public DataSource dataSource() {
-
-        return DataSourceBuilder.create()
-                .url("jdbc:postgresql://localhost:5432/td5_db")
-                .username("postgres")
-                .password("Kokoo271107.")
-                .driverClassName("org.postgresql.Driver")
-                .build();
+        PGSimpleDataSource ds = new PGSimpleDataSource();
+        ds.setUrl("jdbc:postgresql://localhost:5432/td5_db");
+        ds.setUser("postgres");
+        ds.setPassword("Kokoo271107.");
+        return ds;
     }
 }
